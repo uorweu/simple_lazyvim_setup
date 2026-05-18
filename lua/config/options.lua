3dd012ecddd41c58da1c78075aecb5ccb2b0d1be
Norman <nguyengiabao1712006@gmail.com>
2026-05-18 12:06:18 +0700

    First commit

---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+--disable some ui elements-- can see it from <leader> + u
+vim.g.snacks_animate = false --this on is for a - disable animations // see in leader u
---
+++ lua/plugins/disable_plugins.lua
@@ -0,0 +1,5 @@
+return {
+  { "nvim-treesitter/nvim-treesitter", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+  { "folke/ts-comments.nvim", enabled = false },
+}
